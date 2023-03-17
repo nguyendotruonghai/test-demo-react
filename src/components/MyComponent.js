410c22cc04ec579c0d11c492637fc5868afdb808
nguyendotruonghai <kenny220290@gmail.com>
2023-03-17 10:21:39 +0700

    lesson 29

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,38 +6,40 @@ import DisplayInfo from "./DisplayInfo";
 import AddUserInfo from "./AddUserInfo";
 
 class MyComponent extends React.Component {
-    state = {
-        listUsers : [
-            {id : 1, name : "Hai" , age: 16},
-            {id : 2, name : 'Kenny', age: 26},
-            {id : 3, name : 'Roy' , age : 69}
-        ]
-    }
+  state = {
+    listUsers: [
+      { id: 1, name: "Hai", age: 16 },
+      { id: 2, name: "Kenny", age: 26 },
+      { id: 3, name: "Roy", age: 69 },
+    ],
+  };
 
-    handleAddNewUser = (userOjs) => {
-        //bad code
-        // let listUserNew = this.state.listUsers;
-        // listUserNew.unshift(userOjs);
-        // this.setState({
-        //     listUsers : listUserNew
-        // })
-    
-        this.setState({
-            // listUsers: [...this.state.listUsers,userOjs]
-            listUsers: [userOjs,...this.state.listUsers]
-        })
-    }
+  handleAddNewUser = (userOjs) => {
+    //bad code
+    // let listUserNew = this.state.listUsers;
+    // listUserNew.unshift(userOjs);
+    // this.setState({
+    //     listUsers : listUserNew
+    // })
+
+    this.setState({
+      // listUsers: [...this.state.listUsers,userOjs]
+      listUsers: [userOjs, ...this.state.listUsers],
+    });
+  };
 
   //JSX
   render() {
     return (
-      <div>
-        <AddUserInfo handleAddNewUser={this.handleAddNewUser}/>
-        <br/><br/>
-        <DisplayInfo 
-        listUsers={this.state.listUsers}
-        />
-      </div>
+      <>
+        <div className="a">
+          <AddUserInfo handleAddNewUser={this.handleAddNewUser} />
+          <br />
+          <br />
+          <DisplayInfo listUsers={this.state.listUsers} />
+        </div>
+        <div className="b"></div>
+      </>
     );
   }
 }
