4f6f6ba996119b6497a9ef98c8b82b5586767613
nguyendotruonghai <kenny220290@gmail.com>
2023-03-17 22:39:03 +0700

    lesson 35

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,54 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfo.scss";
-import logo from './../logo.svg';
-class DisplayInfo extends React.Component {
-  state = {
-    isShowListUser: true,
+import logo from "./../logo.svg";
+
+//stateless vs stateful
+// class DisplayInfo extends React.Component {
+//     render() {
+//       console.log('call me render');
+//       // cu phap destructuring array/object = const name/age = this.props.name/age
+//       //const {name, age} = this.props;
+//       const { listUsers } = this.props; // const listUsers = this.props.listUsers
+//       return (
+//         <div className="display-info-container">
+//           {true && (
+//             <>
+//                   {listUsers.map((user) => {
+//                 return (
+//                   <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+//                     <div>Id {user.id}</div>
+//                     <div>My name's {user.name}</div>
+//                     <div>My age's {user.age}</div>
+//                     <div>
+//                       <button
+//                         onClick={() => this.props.handleDeleteUser(user.id)}
+//                       >
+//                         Delete
+//                       </button>
+//                     </div>
+//                   </div>
+//                 );
+//                   })}
+//             </>
+//           )}
+//         </div>
+//       );
+//     }
+// }
+
+const DisplayInfo = (props) => {
+  const { listUsers } = props; // const listUsers = this.props.listUsers
+  // cu phap destructuring array/object = const name/age = this.props.name/age
+  //const {name, age} = this.props;
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+  const handleShowHideListUser = () => {
+    setShowHideListUser(!isShowHideListUser);
   };
-  handleShowHide = () => {
-    this.setState({
-      isShowListUser: !this.state.isShowListUser,
-    });
-  };
-  render() {
-    // cu phap destructuring array/object = const name/age = this.props.name/age
-    //const {name, age} = this.props;
-    const { listUsers } = this.props; // const listUsers = this.props.listUsers
-    return (
-      <div className="display-info-container">
-        {/* <img src={logo}/> */}
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser === true
-              ? "Hide list user"
-              : "Show List User"}
-          </span>
-        </div>
-        {this.state.isShowListUser && (
-          <div>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div>
-                    Id {user.id}
-                  </div>
-                  <div>My name's {user.name}</div>
-                  <div>My age's {user.age}</div>
-                  <div>
-                    <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        )}
+
+  return (
+    <div className="display-info-container">
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? "Hide List Users" : "Show List Users"}
+        </span>
       </div>
-    );
-  }
-}
+      {isShowHideListUser && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                <div>Id {user.id}</div>
+                <div>My name's {user.name}</div>
+                <div>My age's {user.age}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfo;
