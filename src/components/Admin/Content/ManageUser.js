44e30be2d64659063e6cfe3a1d23c53f5abee32b
nguyendotruonghai <kenny220290@gmail.com>
2023-03-24 22:30:14 +0700

    lesson 57

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,4 +1,5 @@
 import ModalCreateUser from "./ModalCreateUser";
+import './ManageUser.scss';
 
 const ManageUser = (props) => {
   return (
@@ -10,8 +11,9 @@ const ManageUser = (props) => {
         </div>
         <div>
           table Users
-          <ModalCreateUser />
+         
         </div>
+        <ModalCreateUser />
       </div>
     </div>
   );
